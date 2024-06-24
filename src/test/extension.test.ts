03ff1757b9613c6902e7c22388dffedd01b2f592
itsbrex <roach7@gmail.com>
2024-06-24 14:13:10 -0700

    add import statements: assert, vscode
    This commit adds import statements for assert and vscode at the beginning of the file.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,2 +1,5 @@
-// import * as assert from 'assert';
-// import * as vscode from 'vscode';
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+// dummy code to prevent linting errors
+assert.ok(vscode);
